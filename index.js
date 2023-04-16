c16145b714f7ca04989f7e63ab0b2fd9579edb81
gjainiitr <47331030+gjainiitr@users.noreply.github.com>
2023-04-16 11:12:51 +0530

    Adding logic to import graphql schema
    Now all files with .graphql extension in the entire project will be imported automatically

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const { loadSchemaSync } = require('@graphql-tools/load');
+const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader');
+
+const schema = loadSchemaSync('./schema.graphql', {
+    loaders: [new GraphQLFileLoader()]
+});
+
 const app = express();
 
 app.get('/', (req, res) => {
