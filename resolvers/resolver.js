2192372a8ad44e012f9fcd5a43a24facba700c3b
gjainiitr <47331030+gjainiitr@users.noreply.github.com>
2023-04-16 18:17:32 +0530

    Add functions for mutation and queries. Add dummy data
    Added dummy data and created function templates to interact with dummy data.
    
    Yet to add logic inside functions.

--- resolvers/resolver.js
+++ resolvers/resolver.js
@@ -1,26 +1,80 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const moviesList = [
+    {
+        id: 1,
+        title: "Dangal",
+        year: 2016,
+        rating: 8.4,
+        genres: ["Action", "Biography", "Drama", "Sport"],
+    },
+    {
+        id: 2,
+        title: "Jannat 2",
+        year: 2012,
+        rating: 5.4,
+        genres: ["Action", "Crime", "Drama", "Romance", "Thriller"],
+    }
+];
+
+const reviewsList = [
+    {
+        reviewId: 1,
+        movieId: 2,
+        userId: 111,
+        rating: 3.5,
+        comment: "Best movie ever"
+    }
+];
+
+const getMovies = (parent) => {
+    return moviesList;
+}
+
+//TODO: Implement logic
+const getMovieWithReview = (parent, args) => {
+
+}
+
+const addMovie = (parent, args) => {
+    let movie = args.input;
+    movie.id = uuidv4();
+    moviesList.push(movie);
+    return movie;
+}
+
+const updateMovie = (parent, args) => {
+
+}
+
+const deleteMovie = (parent, args) => {
+
+}
+
+const addReview = (parent, args) => {
+
+}
+
+const updateReview = (parent, args) => {
+
+}
+
+const deleteReview = (parent, args) => {
+
+}
+
 const resolvers = {
     Query: {
-        getMovies: (parent) => {
-            return [
-                {
-                    id: 1,
-                    title: "Dangal",
-                    year: 2016,
-                    rating: 8.4,
-                    genres: ["Action", "Biography", "Drama", "Sport"],
-                },
-                {
-                    id: 2,
-                    title: "Jannat 2",
-                    year: 2012,
-                    rating: 5.4,
-                    genres: ["Action", "Crime", "Drama", "Romance", "Thriller"],
-                }
-            ];
-        }
+        getMovies: getMovies,
+        getMovie: getMovieWithReview
     },
     Mutation: {
-
+        addMovie: addMovie,
+        updateMovie: updateMovie,
+        deleteMovie: deleteMovie,
+        addReview: addReview,
+        updateReview: updateReview,
+        deleteReview: deleteReview
     }
 };
 export {resolvers};
